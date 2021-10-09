79c5263338bf1cba26c535f20debe995cd4c8b5e
linooohon <linooohon@gmail.com>
2021-10-09 15:38:13 +0800

    add discription

--- app/static/index.js
+++ app/static/index.js
@@ -15,11 +15,11 @@ function onSignIn(googleUser) {
 
     let id_token = googleUser.getAuthResponse().id_token;
     let profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log("Name: " + profile.getName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail()); // This is null if the "email" scope is not present.
-    console.log("id_token: " + id_token);
+    // console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log("Name: " + profile.getName());
+    // console.log("Image URL: " + profile.getImageUrl());
+    // console.log("Email: " + profile.getEmail()); // This is null if the "email" scope is not present.
+    // console.log("id_token: " + id_token);
     let body = {
         "id_token": id_token
     }
